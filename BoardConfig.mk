888a3b9d8c969bdb4f2d45f5300f39b768e66351
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    dumpling: Enforce common device tree presence
     * The tree has no chance of building without the common
       device tree as it contains architecture definitions.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@
 #
 
 # Inherit from oneplus msm8998-common
--include device/oneplus/msm8998-common/BoardConfigCommon.mk
+include device/oneplus/msm8998-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/oneplus/dumpling
 
