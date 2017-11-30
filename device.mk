ee9ae03330ad7c25d72b626c48a1fa949201872f
TheScarastic <warabhishek@gmail.com>
2017-11-30 18:31:58 +0530

    dumpling: Add custom biometrics
     * Goodix hal uses old fingeprintd interface to send binder calls
     * Use new biometrics binder calls for fpc
    
    co-authored with:- alexstsv2
    
    amartinz: adjust for dumpling
    
    Change-Id: Ida433860d9f05b652744c9f91e11afe6a5966b87
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- device.mk
+++ device.mk
@@ -62,5 +62,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service-custom \
+
 # Inherit from oneplus msm8998-common
 $(call inherit-product, device/oneplus/msm8998-common/common.mk)
