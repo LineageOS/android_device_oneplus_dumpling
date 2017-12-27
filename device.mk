38266adc4bdaaebb2e9fec4af89ce6f648fbef52
Marko Man <darkobas@gmail.com>
2017-12-27 09:40:46 +0100

    dumpling: add goodix keylayout
    Change-Id: Iebbbca338e6d8223086bcfa2967af47fa27b176d

--- device.mk
+++ device.mk
@@ -66,5 +66,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service-custom
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gf_input.kl:system/usr/keylayout/gf_input.kl
+
 # Inherit from oneplus msm8998-common
 $(call inherit-product, device/oneplus/msm8998-common/common.mk)
