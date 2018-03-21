9c3c20468e4c3a508393a47cb92c0b19db8331a0
XiNGRZ <chenxingyu92@gmail.com>
2018-03-22 00:36:45 +0800

    dumpling: move vendor blobs to /vendor
    ---
    From 3ab8bbb9c1dbd719d830c94cb1384f57f92ff758 Mon Sep 17 00:00:00 2001
    From: XiNGRZ <chenxingyu92@gmail.com>
    Date: Tue, 20 Feb 2018 21:44:47 +0800
    Subject: [PATCH] dumpling: Hex-edit "gf_fingerprint" to "fingerprint"
    
    This commit is inspired by
    https://github.com/sultanxda/android_device_oneplus_dumpling/commit/28ed156634c0947687588101fc62a761297e2dd4
    
    Change-Id: I2645b0ecb8785a7a6bc8718c0a0f8b2e0914b223
    ---

--- extract-files.sh
+++ extract-files.sh
@@ -25,3 +25,18 @@ export VENDOR=oneplus
 export DEVICE_BRINGUP_YEAR=2017
 
 ./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+function fix_goodix () {
+    sed -i \
+        's/\x00gf_fingerprint\x00/\x00fingerprint\x00\x00\x00\x00/' \
+        "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/"$1"
+}
+
+fix_goodix vendor/lib/hw/fingerprint.goodix.so
+fix_goodix vendor/lib64/hw/fingerprint.goodix.so
