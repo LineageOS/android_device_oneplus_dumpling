5bbd7e6a5c5268ae5f2add69b9de45b67a55b7fd
Pranav Vashi <neobuddy89@gmail.com>
2020-03-24 18:27:17 +0530

    dumpling: Move bdroid header to common tree
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I1a595c948b4f849bd6035807dc3e38ad65970064

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,9 +31,6 @@ DEVICE_PATH := device/oneplus/dumpling
 TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := dumpling,OnePlus5T,oneplus5t
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
-
 # inherit from the proprietary version
 -include vendor/oneplus/dumpling/BoardConfigVendor.mk
 
