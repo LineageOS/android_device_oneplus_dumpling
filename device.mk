600800e80216b35a05b748538957d7de23821518
Georg Veichtlbauer <georg@vware.at>
2021-09-02 08:59:59 +0200

    dumpling: Move fingerprint location overlay
    Change-Id: Ie46c3f23654ad00ade85f61284ae2a57a402618f

--- device.mk
+++ device.mk
@@ -25,7 +25,9 @@
 $(call inherit-product, vendor/oneplus/dumpling/dumpling-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
