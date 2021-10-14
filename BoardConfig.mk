f4e77041a6b44129d9f842e1acaa6f08df896739
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-14 18:36:03 +0000

    dumpling: import properties from common init
    Change-Id: If3dcc42d90d29b7d5dd7580f7489a44f084c5430

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,9 @@ DEVICE_PATH := device/oneplus/dumpling
 TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := dumpling,OnePlus5T,oneplus5t
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
